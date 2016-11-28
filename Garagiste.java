a53e0e642d036f74f64430521b457e83aeebd81b
RevLoc <margaux.thoquenne.sio@gmail.com>
2016-11-28 14:33:32 +0100

    Classes Garagiste,attribut

--- Garagiste.java
+++ Garagiste.java
@@ -1,4 +1,11 @@
 private class Garagiste{
 	private String nom ; 
 	private String adresse ;
+
+
+	public Garagiste(string nom , string adresse){
+		this.nom = nom ;
+		this.adresse = adresse ; 
+	}
+
 }
